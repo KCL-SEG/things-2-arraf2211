6742c3bda27b92251d4b3ca890fac3caade7cce4
arraf2211 <arrafrahman22@gmail.com>
2022-12-08 13:36:56 +0000

    tests

--- things/forms.py
+++ things/forms.py
@@ -8,4 +8,5 @@ class ThingForm(forms.ModelForm):
     class Meta:
         model = Thing
         fields = ['name','description','quantity']
+    description = forms.CharField(label="description", widget=forms.Textarea())
     
